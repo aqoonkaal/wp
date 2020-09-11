a45c0eae8ba5bb5d01ce445d145e57a420fdc29b
AqoonKaal <aqoonkaal@gmail.com>
2020-09-11 12:02:40 -0500

    Qoraal kooban

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # WordPress ku baro Af-Soomaali
 
-Halkan waxaad ka daalacan kartaa xeerarka Naqshadda WordPress.
+Halkan waxaad ka daalacan kartaa xeerarka Naqshadda WordPress, gaar ahaan waxaa boggan ku qoran xeerarka casharka 1aad oo aad ka daawan karto Youtube.com/aqoonkaal
--- index.php
+++ index.php
@@ -1,18 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Naqshadda Tababarka</title>
 </head>
 
-<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
-  <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-  <?php the_excerpt(); ?>
-<?php endwhile; ?>
-<?php else: ?>
-  <p><?php _e("Waa ka xumahay qoraalka ma helin.", "aqoonkaal") ?>
-<?php endif; ?>
-
+<p> Cinwaanka qoraalka koowaad </p>
 </body>
 </html>
