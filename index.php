3abc80834559dd208d0e269bc39218b51ea66e42
AqoonKaal <aqoonkaal@gmail.com>
2020-09-14 09:30:24 -0500

    xeerka HTML

--- index.php
+++ index.php
@@ -6,7 +6,6 @@
   <title>Naqshadda Tababarka</title>
 </head>
 <body>
-  
 <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
   <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
   <?php the_excerpt(); ?>
