df3a1f0d54dae0b40105972d4e5dcf8f899addf3
AqoonKaal <aqoonkaal@gmail.com>
2020-09-15 10:23:05 -0500

    Xeerka HTML

---
+++ footer.php
@@ -0,0 +1,7 @@
+
+
+<p>&copy; 2015 - <?php echo date('Y'); ?> AqoonKaal.com</p>
+
+<?php wp_footer(); ?>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Naqshadda Tababarka</title>
-</head>
-<body>
+<?php get_header(); ?>
+
 <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
   <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
   <?php the_excerpt(); ?>
@@ -14,5 +8,4 @@
   <p><?php _e("Waa ka xumahay qoraalka ma helin.", "aqoonkaal") ?>
 <?php endif; ?>
 
-</body>
-</html>
+<?php get_footer(); ?>
